62d67dd6a3cd621e1e2835b469d880758d0265cb
salvasser <al.salvasser@gmail.com>
2021-12-02 08:54:00 +0700

    modified persons.erl and add native_compile.dat

--- HW5/persons.erl
+++ HW5/persons.erl
@@ -1,50 +1,30 @@
 -module(persons).
--export([database/0, age/0, average_age/0, phone/0, find_user/1]).
+-export([database/0, average_age/0, find_user/1]).
 -record(person, {name="", phone, age}).
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 %		User data
 database() ->
-	P1=#person{name = "Jonh", phone = 1115, age = 25},
-	P2=#person{name = "Kevin", phone = 1785, age = 18},
-	P3=#person{name = "Nora", phone = 4695, age = 36},
-	P4=#person{name = "David", phone = 3964, age = 40},
-	P5=#person{name = "Fiona", phone = 9358, age = 15},
-	P6=#person{name = "Andrew", phone = 2574, age = 60},
-	P7=#person{name = "Liza", phone = 7563, age = 34},
-	P8=#person{name = "Stanly", phone = 2745, age = 51},
-	P9=#person{name = "Sara", phone = 4833, age = 12},
-	P10=#person{name = "Dash", phone = 7866, age = 22},
+	[#person{name = "Jonh", phone = 1115, age = 25},
+	#person{name = "Kevin", phone = 1785, age = 18},
+	#person{name = "Nora", phone = 4695, age = 36},
+	#person{name = "David", phone = 3964, age = 40},
+	#person{name = "Fiona", phone = 9358, age = 15},
+	#person{name = "Andrew", phone = 2574, age = 60},
+	#person{name = "Liza", phone = 7563, age = 34},
+	#person{name = "Stanly", phone = 2745, age = 51},
+	#person{name = "Sara", phone = 4833, age = 12},
+	#person{name = "Dash", phone = 7866, age = 22}].
 	
-	[P1, P2, P3, P4, P5, P6, P7, P8, P9, P10].
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
-%		This function returns a list of user ages	
-age() ->
-	Data = database(),
-			
-	age(Data, []).
-		
-		age([], Age) -> Age;
-		age([H|T], Age) -> age(T, [Age | H#person.age]). 
-
-%		This function returns a list of user phones. Not used		
-phone() ->
-	Data = database(),
-	
-	phone(Data, []).
-	
-		phone([], Phone) -> Phone;
-		phone([H|T], Phone) -> phone(T, [Phone | H#person.phone]).
-
-%		This function returns the average age of users
 average_age() ->
-	Age = age(),
+	Data = database(),
 
-	average_age(Age, 0, 0).
+	average_age(Data, 0, 0).
 			
 		average_age([], Acc, Len) -> io:fwrite("Average age of people in the database: ~p~n", [Acc/Len]);
-		average_age([H|T], Acc, Len) -> average_age(H, Acc+T, Len+1).
+		average_age([H|T], Acc, Len) -> average_age(T, Acc+H#person.age, Len+1).
 
 %		This function allows you to find user data by phone number
 find_user(Phone_number) ->
@@ -61,6 +41,26 @@ find_user(Phone_number) ->
 								io:fwrite("Phone: ~p~n", [H#person.phone])
 								end;
 		compare(T, Phone_number, _, _) -> find_user(T, Phone_number).
+		
+		
+		
+%		This function returns a list of user ages. Not used
+%age() ->
+%	Data = database(),
+%			
+%	age(Data, []).
+%		
+%		age([], Age) -> Age;
+%		age([H|T], Age) -> age(T, [H#person.age | Age]). 
+%
+%		This function returns a list of user phones. Not used		
+%phone() ->
+%	Data = database(),
+%	
+%	phone(Data, []).
+%	
+%		phone([], Phone) -> Phone;
+%		phone([H|T], Phone) -> phone(T, [H#person.phone | Phone]).
 		
 
 	
