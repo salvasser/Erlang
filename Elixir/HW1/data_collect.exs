376fe18fa55361d75fa9c8ee1648a4befbd39145
salvasser <al.salvasser@gmail.com>
2022-01-27 16:55:43 +0700

    data collect script

---
+++ Elixir/HW1/data_collect.exs
@@ -0,0 +1,8 @@
+defmodule Collect do
+	
+	name = IO.gets("Enter your name: ") |> String.trim
+	email = IO.gets("Enter your email: ") |> String.trim
+	IO.puts "Your name: #{name}\nYour email: #{email}"
+	
+end
+
