b14c3959c7e7b258e8a1a8d4dcad61ba9720ae6e
salvasser <al.salvasser@gmail.com>
2022-01-27 14:58:57 +0700

    v.3

--- web_users/README.md
+++ web_users/README.md
@@ -1,9 +1,22 @@
 web_users
-=====
+==================================================
+This web application works with a database. 
+There is user data here: their id, name, age and phone number. 
+You can display the data of all users at once, display the data of one user by his ID, display the data of users older or younger than a certain age, as well as delete and add users.
+------------------------------------------------
+Before you start working with the application, you need to execute the following queries in sql:
 
-An OTP application
+	CREATE DATABASE erlang;
+	USE erlang;
 
-Build
------
+or use bash-script "start_script" in "resources" folder.
+------------------------------------------------
+if you run the program again, then execute:
 
-    $ rebar3 compile
+	server:clear_data();		(DROP TABLE users) in sql
+then restart the erlang virtual machine.
+------------------------------------------------
+to run the application, run "server:server()" in the terminal
+enter in the address bar of the browser "localhost:8070".
+------------------------------------------------
+pleasant use:)
--- web_users/src/server.erl
+++ web_users/src/server.erl
@@ -1,24 +1,13 @@
 -module(server).
--export([server/0]).
-
-%%!!!!!!!!!!!!
-%%Before you start working with the application, you need to execute the following queries in sql:
-
-%%	CREATE DATABASE erlang;
-%%	USE erlang;
-
-%%if you run the program again, then execute in sql:
-
-%%	DROP TABLE users;
-%%!!!!!!!!!!!!
+-export([server/0, clear_data/0]).
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 %%server part
 client_next(ClientSocket) ->
 	{ok, Msg} = gen_tcp:recv(ClientSocket, 0),
 	io:format("~p~n", [Msg]),
-	All_id = id_list(),
-	HTML = web_data(handler(Msg), Msg, All_id),
+	AllId = id_list(),
+	HTML = web_data(handler(Msg), Msg, AllId),
 	Response = "HTTP/1.1 200 OK\nContent-Type: text/html; charset=utf-8\nContent-Length: " ++ erlang:integer_to_list(string:length(HTML)) ++ "\n\n" ++ HTML,
 
 	gen_tcp:send(ClientSocket, Response),
@@ -31,86 +20,98 @@ client(ServerSocket) ->
 
 server() ->
 	sql_start(),
-	{ok, ListenSocket} = gen_tcp:listen(8076, [binary, {active, false},{reuseaddr, true}]), %%localhost:8076
+	{ok, ListenSocket} = gen_tcp:listen(8071, [binary, {active, false},{reuseaddr, true}]), %%localhost:8070
 	client(ListenSocket).
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 %%this function processes the user's request	
 handler(Msg) ->
-	List_text = ["get-users", "get-user", "delete-user", "add-user"],
-	[_Unused, Request, _Id | _Tail] = splitting(Msg),	
-	handler(Request, List_text).
+	ListOfText = ["get-users", "get-user", "delete-user", "add-user"],
+	[_, Request, _Id | _Tail] = splitting(Msg),	
+	handler(Request, ListOfText).
 	
 	handler(_Request, []) -> "start-page";
-	handler(Request, [H_text|T_text]) -> 
-		case Request == H_text of
-			true -> H_text;
-			false -> handler(Request, T_text)
+	handler(Request, [HeadText | TailText]) -> 
+		case Request == HeadText of
+			true -> HeadText;
+			false -> handler(Request, TailText)
 		end.
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 %%function call according to the processed user request	
-web_data("start-page", _Msg, _All_id) -> 
-	{ok, Data} = file:read_file("start_page.html"),
+web_data("start-page", _Msg, _AllId) -> 
+	%Query = "start-page",
+	{ok, Data} = file:read_file("resources/start_page.html"),
 	Data;
-web_data("get-users", Msg, _All_id) -> 
-	[Term, Age] = find_number(Msg),
+web_data("get-users", Msg, _AllId) -> 
+	Query = "get-users",
+	[Term, Age] = find_number(Msg, Query),
 	if
 		Term == "younger" -> "<h1>User data</h1><br>" ++ "<table>" ++ output(younger_than(Age)) ++ "</table>";
 		Term == "older" -> "<h1>User data</h1><br>" ++ "<table>" ++ output(older_than(Age)) ++ "</table>";
 		true -> "<h1>User data</h1><br>" ++ "<table>" ++ output(all_users()) ++ "</table>"
 	end;
-web_data("get-user", Msg, All_id) -> 
-	[Term, ID] = find_number(Msg),
-	Number = checkup_id(All_id, ID),
+web_data("get-user", Msg, AllId) -> 
+	Query = "get-user",
+	[Term, ID] = find_number(Msg, Query),
+	Number = checkup_id(AllId, ID),
 	case (not(Number == missing)) and (Term == "id") of
 		true -> "<h1>User " ++ integer_to_list(Number) ++ " data</h1><br>" ++ "<table>" ++ output(one_user_id(Number)) ++ "</table>";
 		false -> "<h3>There is no user with this ID, check the data<h3>"
 	end;
-web_data("delete-user", Msg, All_id) -> 
-	[Term, ID] = find_number(Msg),
-	Number = checkup_id(All_id, ID),
+web_data("delete-user", Msg, AllId) -> 
+	Query = "delete-user",
+	[Term, ID] = find_number(Msg, Query),
+	Number = checkup_id(AllId, ID),
 	case (not(Number == missing)) and (Term == "id") of
 		true -> delete_user(Number),
 			"<h1>Removing a user</h1><br><p>user " ++ integer_to_list(Number) ++ " deleted</p>";
 		false -> "<h3>There is no user with this ID, check the data<h3>"
 	end;
-web_data("add-user", _Msg, All_id) -> 
-	add_user(All_id).
+web_data("add-user", _Msg, AllId) -> 
+	add_user(AllId).
 	
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 %%working with a database
 sql_start() ->
-	{ok, Pid} = mysql:start_link([{host, "localhost"}, {user, "aleksandr"}, {password, "password"}, {database, "erlang"}]),
+	{ok, Pid} = mysql:start_link([{host, "localhost"}, {user, "aleksandr"}, {password, "DLEta3y!"}, {database, "erlang"}]),
 	true = erlang:register(db, Pid),
 	existing_users().
 
 existing_users() ->
 	mysql:query(db, <<"CREATE TABLE users (id VARCHAR(4), first_name VARCHAR(255), phone VARCHAR(4), age INT)">>),
-	mysql:query(db, <<"INSERT INTO users (id, first_name, phone, age) VALUES ('1','Jonh','1115',25), ('2','Kevin','1785',18), ('3', 'Nora', 4695, 36), ('4', 'David', 3964, 40), ('5', 'Fiona', 9358, 15), ('6', 'Andrew', 2574, 60), ('7', 'Lisa', 7563, 34), ('8', 'Stanly', 2745, 51), ('9', 'Sara', 4833, 12), ('10', 'Dash', 7866, 22)">>).
+	{ok, Data} = file:read_file("resources/users.dat"),
+	ListOfData = string:tokens(binary_to_list(Data), "\r\n\,\ "),
+	read(ListOfData).
+	
+		read([]) -> ok;
+		read([HeadId, HeadName, HeadPhone, HeadAge | TailData]) -> 
+			mysql:query(db, <<"INSERT INTO users (id, first_name, phone, age) VALUES (?,?,?,?)">>, [HeadId, HeadName, HeadPhone, list_to_integer(HeadAge)]),								
+			read(TailData).
+	
 	
 id_list() ->
-	{ok, _Key, All_id} = mysql:query(db, <<"SELECT id FROM users">>),
-	make_list(All_id, []).
+	{ok, _Key, AllId} = mysql:query(db, <<"SELECT id FROM users">>),
+	make_list(AllId, []).
 	
 			make_list([], Acc) -> Acc;
-			make_list([H_id | T_id], Acc) -> make_list(T_id, [binary_to_list(list_to_binary(H_id)) | Acc]).
+			make_list([HeadId | TailId], Acc) -> make_list(TailId, [binary_to_list(list_to_binary(HeadId)) | Acc]).
 
 all_users() ->
 	{ok, _Key, Data} = mysql:query(db, <<"SELECT * FROM users">>),
 	Data.
 
-one_user_id(Number_id) ->
-	{ok, _Key, [User_data]} = mysql:query(db, <<"SELECT * FROM users WHERE id = ?">>, [Number_id]),
-	[User_data].
+one_user_id(NumberId) ->
+	{ok, _Key, [UserData]} = mysql:query(db, <<"SELECT * FROM users WHERE id = ?">>, [NumberId]),
+	[UserData].
 
-delete_user(Number_id) ->
-	mysql:query(db, <<"DELETE FROM users WHERE id = ?">>, [Number_id]).
+delete_user(NumberId) ->
+	mysql:query(db, <<"DELETE FROM users WHERE id = ?">>, [NumberId]).
 	
-add_user(All_id) ->
+add_user(AllId) ->
 	{ok, [ID]} = io:fread("Enter ID: ", "~s"),
 	{ok, [Name]} = io:fread("Enter Name: ", "~s"),
 	{ok, [Phone]} = io:fread("Enter Phone: ", "~s"),
 	{ok, [Age]} = io:fread("Enter Age: ", "~d"),
-	Number = checkup_id(All_id, ID),
+	Number = checkup_id(AllId, ID),
 	case Number == missing of
 		true -> mysql:query(db, <<"INSERT INTO users (id, first_name, phone, age) VALUES (?,?,?,?)">>, [ID, Name, Phone, Age]),
 			"<h3>User added</h3>";
@@ -118,24 +119,34 @@ add_user(All_id) ->
 	end.
 	
 younger_than(Age) ->
-	{ok, _Key, User_data} = mysql:query(db, <<"SELECT * FROM users WHERE age < ?">>, [Age]),
-	User_data.
+	{ok, _Key, UserData} = mysql:query(db, <<"SELECT * FROM users WHERE age < ?">>, [Age]),
+	UserData.
 	
 older_than(Age) ->
-	{ok, _Key, User_data} = mysql:query(db, <<"SELECT * FROM users WHERE age > ?">>, [Age]),
-	User_data.
+	{ok, _Key, UserData} = mysql:query(db, <<"SELECT * FROM users WHERE age > ?">>, [Age]),
+	UserData.
+	
+clear_data() ->
+	sql_start(),
+	mysql:query(db, <<"DROP TABLE users">>).
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 %%this function gets a number from a request
-find_number(Msg) ->
-	[_Unused, _Request, Number_text | _Tail] = splitting(Msg),
-	List = string:lexemes(Number_text, "-"),
-	case length(List) == 2 of	%checking the request in the form !"Term-Number"!
-		true -> List;
-		false -> [error, ""]
-	end.
+find_number(Msg, Query) ->
+	ListOfText = splitting(Msg),
+	find_number(ListOfText, Query, []).
+	
+	find_number([Head1, Head2 | Tail], Query, Acc) -> if 
+							Head1 /= Query -> find_number ([Head2 | Tail], Query, Acc);
+							Head1 == Query -> 
+									List = string:lexemes(Head2, "-"),
+									case length(List) == 2 of	%checking the request in the form !"Term-Number"!
+										true -> List;
+										false -> [error, ""]
+									end
+							end.
 %%checking for the existence of an ID
-checkup_id(All_id, Id) ->
-	case lists:member(Id, All_id) of
+checkup_id(AllId, Id) ->
+	case lists:member(Id, AllId) of
 		true -> list_to_integer(Id);
 		false -> missing
 	end.
@@ -149,9 +160,13 @@ output(Data) ->
 	write_data(Data, []).
 	
 	write_data([], Acc) -> ["<tr>", "<td>", "<h4>", "id", "</h4>", "</td>", "<td>", "<h4>", "name", "</h4>", "</td>", "<td>", "<h4>", "phone", "</h4>" "</td>", "<td>", "<h4>", "age", "</h4>", "</td>", "</tr>"] ++ lists:reverse(Acc);
-	write_data([[H_id, H_name, H_phone, H_age]|T_data], Acc) -> 
-		write_data(T_data, ["</tr>", "</td>", integer_to_list(H_age), "<td>", "</td>", H_phone, "<td>", "</td>", H_name, "<td>", "</td>", H_id, "<td>", "<tr>" | Acc]).	
+	write_data([[HeadId, HeadName, HeadPhone, HeadAge] | TailData], Acc) -> 
+		write_data(TailData, ["</tr>", "</td>", integer_to_list(HeadAge), "<td>", "</td>", HeadPhone, "<td>", "</td>", HeadName, "<td>", "</td>", HeadId, "<td>", "<tr>" | Acc]).	
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%	
+
+
+
+			
 
 
 
