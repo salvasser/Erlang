74db72f696e6ef7791f343a2a28595af547e9bf0
salvasser <94372998+salvasser@users.noreply.github.com>
2023-03-30 16:28:13 +0400

    Update factorial.erl

--- HW3/factorial.erl
+++ HW3/factorial.erl
@@ -1,7 +1,11 @@
--module(factorial).
--export([fac/1]).
+#!/usr/bin/env escript
+main([String]) ->
+  N = list_to_integer(String),
+  F = fac(N, 1),
+  io:format("factorial ~w = ~w\n", [N,F]);
 
-fac(N) -> fac(N, 1).
+main(_) ->
+  halt(1).
 
 fac(0, Acc) -> Acc;
-fac(N, Acc) when N>0 -> fac(N-1, N*Acc).
+fac(N, Acc) when N > 0 -> fac(N-1, N*Acc).
